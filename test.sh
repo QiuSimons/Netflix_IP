7254c9477e50624b52e04d326ac1d32823b16215
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2022-09-01 21:47:03 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,10 +1,7 @@
   echo "$(cat /proc/cpuinfo | grep 'model name')"
-  runner_cpu="$(cat /proc/cpuinfo | grep 'model name')"
-  cpu1="C CPU"
-  cpu2="M CPU"
-  result1=$(echo $runner_cpu | grep "${cpu1}")
-  result2=$(echo $runner_cpu | grep "${cpu2}")
-  if [[ "$result1" != "" ]] && [[ "$result2" != "" ]]
+  runner_cpu1="$(cat /proc/cpuinfo | grep 'model name' | grep 'C CPU')"
+  runner_cpu2="$(cat /proc/cpuinfo | grep 'model name' | grep 'M CPU')"
+  if [[ "$runner_cpu1" != "" ]] && [[ "$runner_cpu2" != "" ]]
   then
       echo "$(cat /proc/cpuinfo | grep 'model name')"
   else
