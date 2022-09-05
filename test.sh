381c8634f7f9f9311ccbf3402d2c106c5aedc3dd
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2022-09-05 12:05:01 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,10 +1,10 @@
+#!/bin/bash
+echo "$(cat /proc/cpuinfo | grep 'model name')"
+runner_cpu1="$(cat /proc/cpuinfo | grep 'model name' | grep 'C CPU')"
+runner_cpu2="$(cat /proc/cpuinfo | grep 'model name' | grep 'M CPU')"
+if [[ "$runner_cpu1" != "" ]] && [[ "$runner_cpu2" != "" ]]; then
   echo "$(cat /proc/cpuinfo | grep 'model name')"
-  runner_cpu1="$(cat /proc/cpuinfo | grep 'model name' | grep 'C CPU')"
-  runner_cpu2="$(cat /proc/cpuinfo | grep 'model name' | grep 'M CPU')"
-  if [[ "$runner_cpu1" != "" ]] && [[ "$runner_cpu2" != "" ]]
-  then
-      echo "$(cat /proc/cpuinfo | grep 'model name')"
-  else
-      echo "cpu performance not enough"
-      exit 1
-  fi
+else
+  echo "cpu performance not enough"
+  return 1
+fi
